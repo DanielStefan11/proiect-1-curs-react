2c0854899d91f39c7054ac676630e1f51c1052a6
DanielStefan11 <dannielstefan592@gmail.com>
2020-09-01 14:12:53 +0300

    updated UserAddForm component

--- src/components/UserAddForm.jsx
+++ src/components/UserAddForm.jsx
@@ -1,60 +1,129 @@
-import React from 'react';
-import './UserAddForm.css';
+import React from "react";
+import "./styling/UserAddForm.css";
+
 class UserAddForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            email: '',
-            isGoldClient: false
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      salary: "",
+      isGoldClient: false,
+      nameError: "",
+      emailError: "",
+      salaryError: "",
+    };
+  }
+
+  updateName(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  updateEmail(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  updateSalary(event) {
+    this.setState({ salary: event.target.value });
+  }
+
+  updateIsGoldClient(event) {
+    this.setState({ isGoldClient: event.target.checked });
+  }
+
+  validateForm() {
+    let nameError = "";
+    let emailError = "";
+    const emailModel = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+    let salaryError = "";
+
+    if (this.state.name.length < 3 || !this.state.name) {
+      nameError = "Trebuie să introduci un nume!";
+    }
+    // if (!this.state.email.includes("@")) {
+    //   emailError = "Adresa email nu este validă!";
+    // }
+    if (!emailModel.test(this.state.email)) {
+      emailError = "Adresa email nu este validă!";
+    }
+    if (!this.state.salary) {
+      salaryError = "Trebuie să introduci salariul!";
     }
 
-    updateName(event) {
-        this.setState({name: event.target.value});
+    if (nameError || emailError || salaryError) {
+      this.setState({ nameError, emailError, salaryError });
     }
+  }
 
-    updateEmail(event) {
-        this.setState({email: event.target.value});
-    }
+  render() {
+    const {
+      name,
+      email,
+      salary,
+      isGoldClient,
+      nameError,
+      emailError,
+      salaryError,
+    } = this.state;
 
-    updateIsGoldClient(event) {
-        this.setState({isGoldClient: event.target.checked});
-    }
+    return (
+      <form
+        className="user-add-form"
+        onSubmit={(event) => {
+          this.validateForm();
+          this.props.submitAddForm(event, name, email, salary, isGoldClient);
+        }}
+      >
+        <h2>Adaugă utilizatori</h2>
+        {/* Name */}
+        <label htmlFor="name">Nume:</label>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          value={this.state.name}
+          onChange={(event) => this.updateName(event)}
+        />
+        <div className="errors">{nameError}</div>
 
-    render() {
-        const {name, email, isGoldClient} = this.state;
+        {/* Email */}
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          type="text"
+          name="email"
+          value={this.state.email}
+          onChange={(event) => this.updateEmail(event)}
+        />
+        <div className="errors">{emailError}</div>
 
-        return (
-            <form
-                className="user-add-form"
-                onSubmit={(event) => this.props.submitAddForm(event, name, email, isGoldClient)}
-            >
-                <h2>Adauga utilizatori:</h2>
-                <label htmlFor="name">Nume:</label>
-                <input
-                    type="text"
-                    name="name"
-                    onChange={(event) => this.updateName(event)}
-                />
-                <label htmlFor="email">Email:</label>
-                <input
-                    type="text"
-                    name="email"
-                    onChange={(event) => this.updateEmail(event)}
-                />
-                <label htmlFor="is-gold-client">Client GOLD</label>
-                <input
-                    type="checkbox"
-                    name="is-gold-client"
-                    value="true"
-                    onChange={(event) => this.updateIsGoldClient(event)}
-                />
+        {/* Salary */}
+        <label htmlFor="salary">Salariu (adaugă valută :D):</label>
+        <input
+          id="salary"
+          type="text"
+          name="salary"
+          value={this.state.salary}
+          onChange={(event) => this.updateSalary(event)}
+        />
+        <div className="errors">{salaryError}</div>
 
-                <input type="submit" value="Introdu utilizatorul"/>
-            </form>
-        )
-    }
+        {/* Gold Client */}
+        <label htmlFor="is-gold-client" className="checkbox-container">
+          Client GOLD
+          <input
+            id="is-gold-client"
+            type="checkbox"
+            name="is-gold-client"
+            checked={this.state.isGoldClient}
+            onChange={(event) => this.updateIsGoldClient(event)}
+          />
+        </label>
+
+        <input type="submit" value="Introdu utilizatorul" />
+      </form>
+    );
+  }
 }
 
 export default UserAddForm;
